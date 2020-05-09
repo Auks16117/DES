b8fc34783910968ea736f4e6a01401e130ae00f6
TakumaSuzuki <suzuki.takuma1998.1204@gmail.com>
2020-05-09 21:04:45 +0900

    S1~S8の追加 InitialPermutationTable、ETableの追加 InitialPermutation、ExpansionPermutationの内容変更

--- DES/main.c
+++ DES/main.c
@@ -9,18 +9,6 @@
 #include<stdlib.h>
 #include<string.h>
 
-// S0
-int S0[2][16] = {
-    { 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1 },
-    { 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0 }
-};
-
-// S1
-int S1[2][16] = {
-    { 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1 },
-    { 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1 }
-};
-
 int PC_1[7][8] = {
     {57, 49, 41, 33, 25, 17, 9, 1},
     {58, 50, 42, 34, 26, 18, 10, 2},
@@ -40,6 +28,84 @@ int PC_2[8][6] = {
     {30, 40, 51, 45, 33, 48},
     {44, 49, 39, 56, 34, 53},
     {46, 42, 50, 36, 29, 32}
+};
+
+int InitialPermutationTable[8][8] = {
+    {58, 50, 42, 34, 26, 28, 10, 2},
+    {60, 52, 44, 36, 28, 20, 12, 4},
+    {62, 54, 46, 38, 30, 22, 14, 6},
+    {64, 56, 48, 40, 32, 24, 16, 8},
+    {57, 49, 41, 33, 25, 17, 9, 1},
+    {59, 51, 43, 35, 27, 19, 11, 3},
+    {61, 53, 45, 37, 29, 21, 13, 5},
+    {63, 55, 47, 39, 31, 23, 15, 7}
+};
+
+int ETable[8][6] = {
+    {32, 1, 2, 3, 4, 5},
+    {4, 5, 6, 7, 8, 9},
+    {8, 9, 10, 11, 12, 13},
+    {12, 13, 14, 15, 16, 17},
+    {16, 17, 18, 19, 20, 21},
+    {20, 21, 22, 23, 24, 25},
+    {24, 25, 26, 27, 28, 29},
+    {28, 29, 30, 31, 32, 1}
+};
+
+int S1[4][16] = {
+    {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
+    {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
+    {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
+    {15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}
+};
+
+int S2[4][16] = {
+    {15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
+    {3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5},
+    {0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15},
+    {13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9}
+};
+
+int S3[4][16] = {
+    {10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
+    {13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
+    {13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7},
+    {1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12}
+};
+
+int S4[4][16] = {
+    {7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
+    {13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
+    {10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4},
+    {3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14}
+};
+
+int S5[4][16] = {
+    {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
+    {14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6},
+    {4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14},
+    {11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3},
+};
+
+int S6[4][16] = {
+    {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
+    {10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8},
+    {9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6},
+    {4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13}
+};
+
+int S7[4][16] = {
+    {4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1},
+    {13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6},
+    {1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2},
+    {6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12}
+};
+
+int S8[4][16] = {
+    {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
+    {1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2},
+    {7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8},
+    {2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}
 };
 
 void print(char *c, char *mozi, int len) {
@@ -115,75 +181,29 @@ void PC2(int number, char *inputLeft, char *inputRight, char *outputLeft, char *
     print("subKeyGenerate", subKeyGenerate, 48);
 }
 
-
-// 鍵の生成
-void key_create(char *key, char *key1, char *key2) {
-    int i;
-    char p10[10], ls_1[10], ls_2[10];
-
-    p10[0] = key[2];
-    p10[1] = key[4];
-    p10[2] = key[1];
-    p10[3] = key[6];
-    p10[4] = key[3];
-    p10[5] = key[9];
-    p10[6] = key[0];
-    p10[7] = key[8];
-    p10[8] = key[7];
-    p10[9] = key[5];
-
-    print("置換(P10)", p10, 10);
-
-    for (i = 0; i < 10; i++) {
-        if (i == 0 || i == 5) {
-            ls_1[i + 4] = p10[i];
-        }
-        else {
-            ls_1[i - 1] = p10[i];
+void ExpansionPermutation(char *ipRight, char *expansionPermutation){
+    int i, j;
+    for(i=0;i<8;i++){
+        for(j=0;j<6;j++){
+            expansionPermutation[6 * i + j] = ipRight[ETable[i][j] - 1];
         }
     }
-    print("ビット移動(LS-1)", ls_1, 10);
-
-
-    key1[0] = ls_1[5];
-    key1[1] = ls_1[2];
-    key1[2] = ls_1[6];
-    key1[3] = ls_1[3];
-    key1[4] = ls_1[7];
-    key1[5] = ls_1[4];
-    key1[6] = ls_1[9];
-    key1[7] = ls_1[8];
-
-
-    for (i = 0; i < 10; i++) {
-        if (i == 0 || i == 1 || i == 5 || i == 6) {
-            ls_2[i + 3] = ls_1[i];
-        }
-        else {
-            ls_2[i - 2] = ls_1[i];
-        }
-    }
-    print("ビット移動(LS-2)", ls_2, 10);
-    key2[0] = ls_2[5];
-    key2[1] = ls_2[2];
-    key2[2] = ls_2[6];
-    key2[3] = ls_2[3];
-    key2[4] = ls_2[7];
-    key2[5] = ls_2[4];
-    key2[6] = ls_2[9];
-    key2[7] = ls_2[8];
 }
 
+
 // 初期転置(IP:Initial Permutation)
-void InitialPermutation(char *plain_text, char *ip) {
-    ip[0] = plain_text[1];
-    ip[1] = plain_text[5];
-    ip[2] = plain_text[2];
-    ip[3] = plain_text[0];
-    ip[4] = plain_text[3];
-    ip[5] = plain_text[7];
-    ip[6] = plain_text[4];
-    ip[7] = plain_text[6];
+void InitialPermutation(char *plainText, char *ipLeft, char *ipRight) {
+    int i, j;
+    char ip[64];
+    for(i=0;i<8;i++){
+        for(j=0;j<8;j++){
+            ip[6 * i + j] = plainText[ETable[i][j] - 1];
+        }
+    }
+    for(i = 0; i < 32; i++){
+        ipLeft[i] = ip[i];
+        ipRight[i] = ip[i + 32];
+    }
 }
 
 // 初期配置の反対の操作(IP-1)
@@ -207,27 +227,20 @@ void Permutation(char *sbox_output, char *p4) {
 }
 
 // 関数F
-void function_f(char *r, char *key, char *p4) {
+void function_f(char *r, char *key, char *p4, char *ipRight) {
     int i, input_s0, input_s1;
     char e_p[8], upper_bit[4], under_bit[4], sbox_output[4];
+    char outputExpansionPermutation[48];
 
     // 拡大転置 (E/P:Expansion Permutation)
-    e_p[0] = r[3];
-    e_p[1] = r[0];
-    e_p[2] = r[1];
-    e_p[3] = r[2];
-    e_p[4] = r[1];
-    e_p[5] = r[2];
-    e_p[6] = r[3];
-    e_p[7] = r[0];
-
+    ExpansionPermutation(ipRight, outputExpansionPermutation);
     print("拡大転置(E/P)", e_p, 8);
 
     // XORする
-    for (i = 0; i < 8; i++) {
-        e_p[i] = e_p[i] ^ key[i];
+    for (i = 0; i < 48; i++) {
+        outputExpansionPermutation[i] = outputExpansionPermutation[i] ^ key[i];
     }
-    print("XOR", e_p, 8);
+    print("XOR", outputExpansionPermutation, 48);
 
     // S0とS1に分ける
     for (i = 3; 0 <= i; i--) {
@@ -242,10 +255,6 @@ void function_f(char *r, char *key, char *p4) {
     input_s0 = upper_bit[0] * 8 + upper_bit[3] * 4 + upper_bit[1] * 2 + upper_bit[2];
     input_s1 = under_bit[0] * 8 + under_bit[3] * 4 + under_bit[1] * 2 + under_bit[2];
 
-    sbox_output[0] = S0[0][input_s0];
-    sbox_output[1] = S0[1][input_s0];
-    sbox_output[2] = S1[0][input_s1];
-    sbox_output[3] = S1[1][input_s1];
     print("Sbox", sbox_output, 4);
     Permutation(sbox_output, p4);
     print("転置(P4)", p4, 4);
@@ -261,9 +270,10 @@ void xor(char *sbox_output, char *l, char *output) {
 }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    int i, password;
+    int i, j, k, password;
     char key[64];
+    char ipLeft[32], ipRight[32];
+    char plainText[64];
     char permutedChoice1Left[28], permutedChoice1Right[28];
     char permutedChoice2Left[28], permutedChoice2Right[28];
     char outputPC2[48];
@@ -279,7 +289,12 @@ int main(int argc, const char * argv[]) {
     }
     printf("\n");
     PC1(key, permutedChoice1Left, permutedChoice1Right);
-    PC2(0, permutedChoice1Left, permutedChoice1Right, permutedChoice2Left, permutedChoice2Right, outputPC2);
+    InitialPermutation(plainText, ipLeft, ipRight);
+    
+    for(k = 0;k < 16;k++){
+        PC2(0, permutedChoice1Left, permutedChoice1Right, permutedChoice2Left, permutedChoice2Right, outputPC2);
+        
+    }
     
     /*
     int i, password;
